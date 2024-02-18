f31b0804509db171361fcf2ca4e4e7005594b4b8
Snehashis Dasgupta <snehashis.jisu.cse@gmail.com>
2024-02-19 01:22:14 +0500

    Pinterest site for uploading pics with authentication

---
+++ public/javascripts/script.js
@@ -0,0 +1,5 @@
+function toggleForm() {
+    const form = document.getElementById("uploadForm");
+    form.style.display = (form.style.display === "none" || form.style.display === "") ? "block" : "none";
+  }
+  
